f6f7edd3d471f3211618cf51d53d84d4b8d83b57
Oliver Foster <oliver.foster@kineo.com>
2015-07-07 21:30:12 +0100

    Update clearlogs.js

--- buildkit/plugins/clearlogs.js
+++ buildkit/plugins/clearlogs.js
@@ -11,16 +11,6 @@ module.exports = new Plugin({
 		});
 	},
 
-	_displayedCourseTechSpec: {},
-
-	"config:loaded": function(config) {
-		//console.log("config:loaded", config);
-	},
-
-	"actions:setup": function(actions) {
-		//console.log("actions:setup", actions);
-	},
-
 	"actions:build": function(actions) {
 
 		this._logsCleared = {};
@@ -41,30 +31,6 @@ module.exports = new Plugin({
 
 		}
 
-	},
-
-	"actions:phase": function(phaseName) {
-		//console.log("actions:phase", phaseName);
-	},
-
-	"action:prep": function(options, action) {		
-		//console.log("action:prep", options, action);
-	},
-
-	"action:start": function(options, action) {
-		//console.log("action:start", options, action);
-	},
-
-	"action:error": function(options, error, action) {
-		//console.log("action:error", options, error);
-	},
-
-	"action:end": function(options, action) {
-		//console.log("action:end", options);
-	},
-
-	"actions:wait": function() {
-		//console.log("actions:wait");
 	}
 
 })
