1cd26c0d314cc2729313fceb729e307f52d3ff6c
Oliver Foster <oliver.foster@kineo.com>
2015-07-07 22:10:54 +0100

    Update techspec.js

--- buildkit/actions/techspec.js
+++ buildkit/actions/techspec.js
@@ -34,8 +34,6 @@ var techspec = new Action({
             }
         }
 
-        console.log("isSupported", probe.isSupported());
-
         if (!probe.isSupported()) {
         	if (options.course) {
             	log(options.course + " - TechSpec: " + "Cannot find video+audio information. Unsupported platform " + os.platform(), 1);
