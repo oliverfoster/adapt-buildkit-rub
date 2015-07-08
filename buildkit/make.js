ddbdcf6cfe942502ad1b906aefbfcc96a3df1a4e
Oliver Foster <oliver.foster@kineo.com>
2015-07-08 11:00:42 +0100

    Added node_modules check

--- buildkit/make.js
+++ buildkit/make.js
@@ -1,4 +1,12 @@
 var terminal = require("./libraries/terminal.js");
 var runner = require("./libraries/runner.js");
 
+var fs = require("fs");
+var path = require("path");
+
+if (!fs.existsSync(path.join(path.dirname(__dirname), "buildkit/node_modules" ))) {
+	console.log("Please run 'npm install' in the buildkits folder");
+	return;
+}
+
 runner.entryPoint( terminal.entryPoint( {} ) );
