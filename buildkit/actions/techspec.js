aeac6063f819f0e390f768b50e42fbaec995ba29
Oliver Foster <oliver.foster@kineo.com>
2015-07-08 12:35:30 +0100

    Added tech spec failed warning

--- buildkit/actions/techspec.js
+++ buildkit/actions/techspec.js
@@ -35,10 +35,10 @@ var techspec = new Action({
         }
 
         if (!probe.isSupported()) {
-        	if (options.course) {
-            	log(options.course + " - TechSpec: " + "Cannot find video+audio information. Unsupported platform " + os.platform(), 1);
+            if (options.course) {
+                log(options.course + " - TechSpec: " + "Cannot find video+audio information. Unsupported platform " + os.platform(), 1);
             } else {
-            	log("TechSpec: Cannot find video+audio information. Unsupported platform " + os.platform(), 1);
+                log("TechSpec: Cannot find video+audio information. Unsupported platform " + os.platform(), 1);
             }
 
             done(options);
@@ -60,17 +60,17 @@ var techspec = new Action({
 
         function checkCallback(file, options) {
 
-        	var extension = path.extname(file.path+"").substr(1); 
+            var extension = path.extname(file.path+"").substr(1); 
 
-        	totalSize += file.size;
+            totalSize += file.size;
 
 
-        	
+            
             if (options.techspec.extensions && options.techspec.extensions[extension]) {
                 var settings = options.techspec.extensions[extension];
                 
-            	file.flaggedProps = [];
-            	
+                file.flaggedProps = [];
+                
                 if ( file.size > textSizeToBytes(settings.size)) {
                     file.flaggedProps.push("filesize: " + bytesSizeToString(file.size, "KB"));
                 } 
@@ -93,73 +93,84 @@ var techspec = new Action({
                     file.flaggedProps.push("video fps: " + file.video_fps);
                 }
                 if (settings.video_codec) {
-	                if (settings.video_codec instanceof Array) {
-	                	if (settings.video_codec.indexOf(file.video_codec) == -1) {
-	             			file.flaggedProps.push("video codec: " + file.video_codec);   		
-	                	}
-	                } else {
-	                	if (settings.video_codec != file.video_codec) {
-	             			file.flaggedProps.push("video codec: " + file.video_codec);   		
-	                	}
-	                }
-	            }
-	            if (settings.audio_codec) {
-	                if (settings.audio_codec instanceof Array) {
-	                	if (settings.audio_codec.indexOf(file.audio_codec) == -1) {
-	             			file.flaggedProps.push("audio codec: " + file.audio_codec);   		
-	                	}
-	                } else {
-	                	if (settings.audio_codec != file.audio_codec) {
-	             			file.flaggedProps.push("audio codec: " + file.audio_codec);   		
-	                	}
-	                }
-	            }
-	            if (options.techspec.restrictedExtensions) {
-	            	if (options.techspec.restrictedExtensions.indexOf(extension) > -1) {
-	            		file.flaggedProps.push("extension:" + extension);   		
-	            	}
-	            }
+                    if (settings.video_codec instanceof Array) {
+                        if (settings.video_codec.indexOf(file.video_codec) == -1) {
+                            file.flaggedProps.push("video codec: " + file.video_codec);         
+                        }
+                    } else {
+                        if (settings.video_codec != file.video_codec) {
+                            file.flaggedProps.push("video codec: " + file.video_codec);         
+                        }
+                    }
+                }
+                if (settings.audio_codec) {
+                    if (settings.audio_codec instanceof Array) {
+                        if (settings.audio_codec.indexOf(file.audio_codec) == -1) {
+                            file.flaggedProps.push("audio codec: " + file.audio_codec);         
+                        }
+                    } else {
+                        if (settings.audio_codec != file.audio_codec) {
+                            file.flaggedProps.push("audio codec: " + file.audio_codec);         
+                        }
+                    }
+                }
+                if (options.techspec.restrictedExtensions) {
+                    if (options.techspec.restrictedExtensions.indexOf(extension) > -1) {
+                        file.flaggedProps.push("extension:" + extension);           
+                    }
+                }
             }
             
             if (file.flaggedProps && file.flaggedProps.length > 0) {
-            	suspects.push(file);
+                suspects.push(file);
             }
 
-        	totalChecked ++;
-        	if (totalChecking === totalChecked) {
-	        	if (suspects.length > 0) {
-		            for (var i = 0, l = suspects.length; i < l; i++) {
-		                var shortenedPath = (suspects[i].path+"").substr(options.src.length+1).replace(/\\/g, "/");
-		                if (options.course) {
-		                	log(options.course + " - TechSpec: Failed [" + suspects[i].flaggedProps.join(", ") + "] "+ shortenedPath, 2);
-		                } else {
-		                	log("TechSpec: Failed [" + suspects[i].flaggedProps.join(", ") + "] " + shortenedPath, 2);
-		                }
-		            }
-		        }
+            totalChecked ++;
+            if (totalChecking === totalChecked) {
+                var failed = false;
+                if (suspects.length > 0) {
+                    failed = true;
+                    for (var i = 0, l = suspects.length; i < l; i++) {
+                        var shortenedPath = (suspects[i].path+"").substr(options.src.length+1).replace(/\\/g, "/");
+                        if (options.course) {
+                            log(options.course + " - TechSpec: Failed [" + suspects[i].flaggedProps.join(", ") + "] "+ shortenedPath, 2);
+                        } else {
+                            log("TechSpec: Failed [" + suspects[i].flaggedProps.join(", ") + "] " + shortenedPath, 2);
+                        }
+                    }
+                }
 
-		        if (options.techspec.totalSize) {
-		        	if (totalSize > textSizeToBytes(options.techspec.totalSize)) {
-		        		if (options.course) {
-		                	log(options.course + " - TechSpec: Failed Total Size " + bytesSizeToString(totalSize, "MB"), 2);                        
-		                } else {
-		                	log("TechSpec: Failed Total Size " + bytesSizeToString(totalSize, "MB"), 2);                        
-		                }
-		        	}
-		        }
+                if (options.techspec.totalSize) {
+                    if (totalSize > textSizeToBytes(options.techspec.totalSize)) {
+                        failed = true;
+                        if (options.course) {
+                            log(options.course + " - TechSpec: Failed Total Size " + bytesSizeToString(totalSize, "MB"), 2);                        
+                        } else {
+                            log("TechSpec: Failed Total Size " + bytesSizeToString(totalSize, "MB"), 2);                        
+                        }
+                    }
+                }
 
-		        done(options);
-		    }
+                if (failed) {
+                    if (options.course) {
+                        logger.log(options.course + " - TechSpec: Please check log file", 1); 
+                    } else {
+                        logger.log("TechSpec: Please check log file", 1); 
+                    }
+                }
+
+                done(options);
+            }
         }
 
 
         function checkFile(file, options) {
 
-			var extension = path.extname(file.path+"").substr(1); 
+            var extension = path.extname(file.path+"").substr(1); 
 
-			var async = false;      	
+            var async = false;          
 
-        	switch ( extension ) {
+            switch ( extension ) {
             case "jpeg":
             case "gif":
             case "jpg":
@@ -178,48 +189,48 @@ var techspec = new Action({
             case "mp3":
             case "ogv":
             case "ogg":
-            	if (probe.isSupported()) {
-	            	async = true;
+                if (probe.isSupported()) {
+                    async = true;
 
-	                file.width = 0;
-	                file.height = 0;
+                    file.width = 0;
+                    file.height = 0;
 
-	                var track = file.path;
-					probe(track, function(err, probeData) {
+                    var track = file.path;
+                    probe(track, function(err, probeData) {
 
-						var video = pluckStream(probeData, "video");
-						var audio = pluckStream(probeData, "audio");
+                        var video = pluckStream(probeData, "video");
+                        var audio = pluckStream(probeData, "audio");
 
-						if (video) {
-							file.width = video.width;
-							file.height = video.height;
-							if (video.bit_rate !== "N/A") {
-								file.video_bitrate = video.bit_rate;
-							}
-							if (video.r_frame_rate.indexOf("/")) {
-								file.video_fps = eval(video.r_frame_rate);
-							} else if (video.avg_frame_rate.indexOf("/")) {
-								file.video_fps = eval(video.avg_frame_rate);
-							}
-							file.video_codec = video.codec_name;
-						} 
+                        if (video) {
+                            file.width = video.width;
+                            file.height = video.height;
+                            if (video.bit_rate !== "N/A") {
+                                file.video_bitrate = video.bit_rate;
+                            }
+                            if (video.r_frame_rate.indexOf("/")) {
+                                file.video_fps = eval(video.r_frame_rate);
+                            } else if (video.avg_frame_rate.indexOf("/")) {
+                                file.video_fps = eval(video.avg_frame_rate);
+                            }
+                            file.video_codec = video.codec_name;
+                        } 
 
-						if (audio) {
-							file.audio_bitrate = audio.bit_rate;
-							if (audio.bit_rate !== "N/A") {
-								file.audio_bitrate = audio.bit_rate;
-							}
-							file.audio_codec = audio.codec_name;
-						}
+                        if (audio) {
+                            file.audio_bitrate = audio.bit_rate;
+                            if (audio.bit_rate !== "N/A") {
+                                file.audio_bitrate = audio.bit_rate;
+                            }
+                            file.audio_codec = audio.codec_name;
+                        }
 
-						checkCallback(file, options);
+                        checkCallback(file, options);
 
-					});
-	                break
-	            } else {
-	            	file.width = 0;
-	                file.height = 0;
-	            }
+                    });
+                    break
+                } else {
+                    file.width = 0;
+                    file.height = 0;
+                }
             }
 
 
@@ -227,40 +238,40 @@ var techspec = new Action({
         }
 
         function pluckStream(probeData, codec_type) {
-        	return _.findWhere(probeData.streams, {codec_type:codec_type});
+            return _.findWhere(probeData.streams, {codec_type:codec_type});
         }
 
         function textSizeToBytes(str) {
-        	str = (str+"");
-        	var sizes = [ "b", "kb", "mb", "gb" ];
-        	var sizeIndex = 0;
-        	var lcStr = str.toLowerCase();
-        	for (var i = sizes.length - 1, l = -1; i > l; i--) {
-        		if (lcStr.indexOf(sizes[i]) != -1) {
-        			sizeIndex = i;
-        			break;
-        		}
-        	}
+            str = (str+"");
+            var sizes = [ "b", "kb", "mb", "gb" ];
+            var sizeIndex = 0;
+            var lcStr = str.toLowerCase();
+            for (var i = sizes.length - 1, l = -1; i > l; i--) {
+                if (lcStr.indexOf(sizes[i]) != -1) {
+                    sizeIndex = i;
+                    break;
+                }
+            }
 
-        	var multiplier = (str.indexOf("B") > -1) ? 1024 : 1000;
+            var multiplier = (str.indexOf("B") > -1) ? 1024 : 1000;
 
-        	var num = parseInt(str);
+            var num = parseInt(str);
 
-        	var rtn = num * Math.pow(multiplier, sizeIndex);
+            var rtn = num * Math.pow(multiplier, sizeIndex);
 
-        	return rtn;
+            return rtn;
         }
 
         function bytesSizeToString(number, size) {
-        	
-        	var sizes = [ "b", "kb", "mb", "gb" ];
-        	var sizeIndex = sizes.indexOf(size.toLowerCase());
-        	var multiplier = (size.indexOf("B") > -1) ? 1024 : 1000;
-        	if (sizeIndex == -1) sizeIndex = 0;
+            
+            var sizes = [ "b", "kb", "mb", "gb" ];
+            var sizeIndex = sizes.indexOf(size.toLowerCase());
+            var multiplier = (size.indexOf("B") > -1) ? 1024 : 1000;
+            if (sizeIndex == -1) sizeIndex = 0;
 
-        	var rtn = (Math.round( (number/ Math.pow(multiplier, sizeIndex) ) * 100) / 100) + size;
+            var rtn = (Math.round( (number/ Math.pow(multiplier, sizeIndex) ) * 100) / 100) + size;
 
-        	return rtn;
+            return rtn;
 
         }
 
