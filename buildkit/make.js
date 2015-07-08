9777c9131c0836a8610dff8d68436503901b8177
Oliver Foster <oliver.foster@kineo.com>
2015-07-08 11:14:29 +0100

    Fixed node_modules warning

--- buildkit/make.js
+++ buildkit/make.js
@@ -1,12 +1,12 @@
-var terminal = require("./libraries/terminal.js");
-var runner = require("./libraries/runner.js");
-
 var fs = require("fs");
 var path = require("path");
 
 if (!fs.existsSync(path.join(path.dirname(__dirname), "buildkit/node_modules" ))) {
-	console.log("Please run 'npm install' in the buildkits folder");
+	console.log("Please run 'npm install' in the 'buildkit' folder");
 	return;
 }
+
+var terminal = require("./libraries/terminal.js");
+var runner = require("./libraries/runner.js");
 
 runner.entryPoint( terminal.entryPoint( {} ) );
