8ad4a2c6f7648c5579a1a35a50d2f839934a22af
Oliver Foster <oliver.foster@kineo.com>
2015-07-16 14:17:41 +0100

    Update make.js

--- buildkit/make.js
+++ buildkit/make.js
@@ -2,7 +2,9 @@ var fs = require("fs");
 var path = require("path");
 
 if (!fs.existsSync(path.join(path.dirname(__dirname), "buildkit/node_modules" ))) {
-	console.log("Please run 'npm install' in the 'buildkit' folder");
+	console.log("Please run 'adapt-buildkit install rub'.\n")
+	console.log("Note: You may need to install adapt-buildkits.")
+	console.log("If so, please run 'sudo npm install -g adapt-buildkits'.");
 	return;
 }
 
