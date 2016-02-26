b3acbd6294433637d61ba0f9ed9cec276ab284f2
Oliver Foster <oliver.foster@kineo.com>
2016-02-26 16:03:11 +0000

    Warn node upgrade

--- buildkit/make.js
+++ buildkit/make.js
@@ -9,6 +9,23 @@ if (!fs.existsSync(path.join(path.dirname(__dirname), "buildkit/node_modules" ))
 }
 
 try {
+
+
+/*UPDATE TO 4.3.0*/
+var chalk = require("chalk");
+var fs = require('fs');
+var path = require('path');
+var semver = require('semver');
+var curVer = process.versions.node;
+var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json')));
+var versRange = pkg.engines.node;
+if(semver.satisfies(curVer, pkg.engines.node) === false && pkg.custom !== true){
+	console.log(chalk.red('WARNING!! URGENT ACTION REQUIRED!!'));
+	console.log(chalk.red('The next version of rub is only compatible with node versions '+pkg.engines.node+', you are curently using ' + curVer + ' - errors will occur.'));
+	console.log(chalk.red("If you don't know how to upgrade node, please email or speak to to oliver.foster@kineo.com"));
+}
+
+
 	
 var terminal = require("./libraries/terminal.js");
 var runner = require("./libraries/runner.js");
