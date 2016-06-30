2b9315b7272754a5734c5556486ed2e43434f3c1
Oliver Foster <oliver.foster@kineo.com>
2016-06-30 14:13:50 +0100

    Fixed global and course exclusions

--- buildkit/plugins/buildkit-plugin-adapt-folderExclusions/js/index.js
+++ buildkit/plugins/buildkit-plugin-adapt-folderExclusions/js/index.js
@@ -26,15 +26,19 @@ class Plugin {
 
 		if (this.config.folderexclusions.global) {
 			if (_.keys(this.config.folderexclusions.global).length > 0) {
-				logger.noticeThrough("Exclusions:"+_.reduce(this.config.folderexclusions.global, function(memo, value){ return memo += " " + value; }, ""));
+				logger.noticeThrough("Global Exclusions:"+_.reduce(this.config.folderexclusions.global, function(memo, value){ return memo += " " + value; }, ""));
 			}
 		}
 		if (this.config.folderexclusions.course) {
 			if (_.keys(this.config.folderexclusions.course).length > 0) {
 				for (var course in this.config.folderexclusions.course) {
-					if (!_.contains(this.config.terminal.courses, course)) continue;
-					if (_.keys(this.config.folderexclusions.course[course]).length > 0) {
+					if (this.config.terminal.courses.length === 0) {
 						logger.noticeThrough(course +" - Exclusions:"+_.reduce(this.config.folderexclusions.course[course], function(memo, value){ return memo += " " + value; }, ""));
+					} else {
+						if (!_.contains(this.config.terminal.courses, course)) continue;
+						if (_.keys(this.config.folderexclusions.course[course]).length > 0) {
+							logger.noticeThrough(course +" - Exclusions:"+_.reduce(this.config.folderexclusions.course[course], function(memo, value){ return memo += " " + value; }, ""));
+						}
 					}
 				}
 			}
@@ -55,12 +59,20 @@ class Plugin {
 					action.folderexclusions = _.deepExtend({}, this.config.folderexclusions.course[course]);
 					action.folderexclusions = _.map(action.folderexclusions, function(item) {
 						return "!src/*/"+item;
-					})
-					continue;
+					});
 				}
 			}
 			
-			action.folderexclusions = _.deepExtend({}, this.config.folderexclusions.global);
+			if (this.config.folderexclusions.global && _.keys(this.config.folderexclusions.global).length > 0) {
+				var globalfolderexclusions = _.deepExtend({}, this.config.folderexclusions.global);
+				globalfolderexclusions = _.map(globalfolderexclusions, function(item) {
+					return "!src/*/"+item;
+				});
+
+				action.folderexclusions = action.folderexclusions || [];
+				action.folderexclusions = action.folderexclusions.concat(globalfolderexclusions);
+			}
+
 		}
 	}
 }
